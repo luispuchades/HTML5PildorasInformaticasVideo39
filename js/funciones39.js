1d510b2efe940716838d8cd8b192ab98c0040123
Luis Puchades <luispuchades@live.com>
2016-02-23 06:53:03 +0100

    HTML5PildorasInformaticasVideo39

---
+++ js/funciones39.js
@@ -0,0 +1,43 @@
+///*global window*/
+/*jslint browser: true, for:true */
+
+/**JavaScript Document
+ * Curso: HMTL5 - Pildoras Informáticas - API CANVAS I
+ * Origin: Capitulo4.html ==> Añadir y quitar clases desde JavaScript
+ */
+
+// "use strict";
+
+
+//1. Definición de Objetos y Variables
+var elemOrigen;
+
+elemOrigen = document.getElementById("imagen");
+
+
+//1.1 Extracción de elementos desde HTML
+
+
+//2. Definición de Funciones
+function comenzar() {
+    elemOrigen.addEventListener("dragstart", function () {
+        alert("Comenzó el evento dragstart");
+    },false)
+}
+
+//DRAGEND
+//function comenzar() {
+//    elemOrigen.addEventListener("dragend", function () {
+//        alert("Se activó el evento dragend");
+//    },false)
+//}
+
+//DRAG
+//function comenzar() {
+//    elemOrigen.addEventListener("drag", function () {
+//        alert("Se activó el evento drag");
+//    },false)
+//}
+
+//3. Asignación de Eventos
+document.addEventListener("DOMContentLoaded", comenzar, false);
